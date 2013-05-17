e74da21f1820495a32b33546769e5ade78ddf34f
Peter <pwshugar@gmail.com>
2013-05-17 15:51:20 -0700

    stringifyJSON incomplete update

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,20 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   // your code goes here
+  var arr = [];
+  for (var i = 0; i < obj.length; i++){
+  	if (typeof obj[i] === 'string') arr.push('"' + obj[i] + '"');	
+  	else if (Array.isArray(obj[i])) arr.push("[" + obj[i] + "]");
+  	else if (Object.prototype.toString.call(obj[i])) {
+      var keys = [];
+      var objArr = [];
+      for (var k in obj[i]) keys.push(k);
+      for (var j = 0; j < keys.length; j++) {
+      	objArr.push(keys(j) + ": " + keys.j);
+      }
+      arr.push("{" + objArr + "}");
+    }
+	else arr.push("" + obj[i] + "");
+  }
+  return "[" + arr + "]";
 };
