da05db4222fd8eaacfb46493c75cb0c3038c8f56
Peter <pwshugar@gmail.com>
2013-05-19 14:55:23 -0700

    Fixed objReturn, refactored some, test passed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,30 +7,21 @@ var stringifyJSON = function (obj) {
   if (Array.isArray(obj)) return arrReturn(obj);
   else if (obj === null) return "null";
   else if (typeof obj === 'object') return objReturn(obj);
-  else if (typeof obj === 'number') return "" + obj;
-  else if (typeof obj === 'boolean') return "" + obj;
   else if (typeof obj === 'string') return '"' + obj + '"';
-  else if (typeof obj === 'undefined') return "";
+  else return "" + obj;
 };
 
 var arrReturn = function(arr) {
   var resArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    resArr.push(stringifyJSON(arr[i]));
-  }
+  for (var i = 0; i < arr.length; i++) resArr.push(stringifyJSON(arr[i]));
   return "[" + resArr + "]";
 };
 
 var objReturn = function(obj) {
-  var keys = [];
   var objArr = [];
-  var key;
-  var value;
-  for (var k in obj) keys.push(k);
-  for (var i = 0; i < keys.length; i++) {
-    key = stringifyJSON(key[i]);
-    value = stringifyJSON(obj[key[i]]);
-    objArr.push(key + ": " + value);
+  for (var k in obj) {
+    if (typeof obj[k] === 'function') return "{}";
+    objArr.push(stringifyJSON(k) + ":" + stringifyJSON(obj[k]));
   }
-  return ("{" + objArr + "}");
+  return "{" + objArr + "}";
 };
